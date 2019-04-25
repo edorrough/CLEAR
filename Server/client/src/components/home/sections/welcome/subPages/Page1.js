1c08c39c9ece44f77272480b897a59572408aa69
jamu0075 <jamu0075@colorado.edu>
2019-04-25 17:58:35 -0600

    Homepage banner update

--- Server/client/src/components/home/sections/welcome/subPages/Page1.js
+++ Server/client/src/components/home/sections/welcome/subPages/Page1.js
@@ -7,6 +7,7 @@ class Page1 extends Component {
         return (
             <div className="page1-container">
                 <div className="content">
+                  <img className="home-banner" src="https://res.cloudinary.com/dtf7zeh9v/image/upload/v1555968130/background_in_frontend/home-landing/desktop/home-landing.jpg"></img>
                     <div className="page1-left-content">
                     </div>
                     <div className="page1-right-content">
