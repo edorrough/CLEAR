1baf471f3abf44c063b8c055cf0b65848617919f
jamu0075 <jamu0075@colorado.edu>
2019-04-25 17:32:27 -0600

    Home banner now sizes up to 1920x1080(Image size is unusual and will distort past those bounds. Mobile banner adjusted to support iPhone 5 and newer.

--- Server/client/src/components/home/sections/welcome/subPages/Page1.js
+++ Server/client/src/components/home/sections/welcome/subPages/Page1.js
@@ -11,15 +11,7 @@ class Page1 extends Component {
     render() {
         return (
             <div className="page1-container">
-                <div className="content">
-                    <div className="page1-left-content">
-                    </div>
-                    <div className="page1-right-content">
-
-                    </div>
-
-                </div>
-
+              <img className="home-banner" src="https://res.cloudinary.com/dtf7zeh9v/image/upload/v1555968130/background_in_frontend/home-landing/desktop/home-landing.jpg"></img>
             </div>
         )
     }
