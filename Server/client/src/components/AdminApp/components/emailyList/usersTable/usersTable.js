5f10e025b6ef59df51dfdc52fe1a0972899025a3
Chenhao Cheng <chch6597@colorado.edu>
2019-04-25 23:00:29 -0600

    Found a little bug of pencial icon not shown, fixed

--- Server/client/src/components/AdminApp/components/emailyList/usersTable/usersTable.js
+++ Server/client/src/components/AdminApp/components/emailyList/usersTable/usersTable.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-
 export default function UsersTable({ user, deleteUser }) {
-    // debugger
     return (
         <tr>
             <td>{user.firstname}</td>
